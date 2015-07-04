4015c3a1a1a309520520f8cda3e4e33afaa83bdc
Ninad <ninad.mahajan@tcs.com>
2015-07-04 09:53:43 +0530

    Home page APi binding

--- src/app/main/modules/home/home.controller.js
+++ src/app/main/modules/home/home.controller.js
@@ -1,6 +1,46 @@
 'use strict';
 
 angular.module('renu')
-  .controller('HomeCtrl', function ($scope) {
-     
-  });
+ .controller('HomeCtrl', ['$scope','$log','httpRequest', function($scope,$log,httpRequest){
+ 		$scope.recentOrders=[];
+ 		$scope.recentReceived=[];
+ 		$scope.recentLowStocks=[];
+ 		$scope.recentDeliveries=[];
+ 		
+ 		var getRecentOrders=function(){
+ 			httpRequest.get(config.api.recentOrders)
+ 			.then(function(response){
+ 				$log.log(response);
+ 				$scope.recentOrders=response.data;
+ 			});
+ 		};
+ 		var getRecentReceived=function(){
+ 			httpRequest.get(config.api.recentReceived)
+ 			.then(function(response){
+ 				$scope.recentReceived=response.data;
+ 				$log.log(response);
+ 			});
+ 		};
+ 		var getRecentLowStocks=function(){
+ 			httpRequest.get(config.api.recentLowStocks)
+ 			.then(function(response){
+ 				$scope.recentLowStocks=response.data;
+ 				$log.log(response);
+ 			});
+ 		};
+ 		var getRecentDeliveries=function(){
+ 			httpRequest.get(config.api.recentDeliveries)
+ 			.then(function(response){
+ 				$scope.recentDeliveries=response.data;
+ 				$log.log(response);
+ 			});
+ 		};
+
+ 		var loadHomePage=function(){
+ 			getRecentOrders();
+ 			getRecentReceived();
+ 			getRecentLowStocks();
+ 			getRecentDeliveries();
+ 		};
+ 		loadHomePage();
+ }]);
